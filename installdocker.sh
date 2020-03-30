fb04796e6028dc9e7179bfbd4527ab14b2792713
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-03-30 22:34:27 +0200

    Update installdocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -53,7 +53,7 @@ sudo apt-get install \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common
+    software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
