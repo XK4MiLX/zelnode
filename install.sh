ddffa8670058c5be02b7eaaad98341d1ef80dc18
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-04-01 12:53:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -89,7 +89,7 @@ function wipe_clean() {
     sudo rm -rf zelflux && sleep 1
     sudo rm -rf ~/$CONFIG_DIR/determ_zelnodes ~/$CONFIG_DIR/sporks ~/$CONFIG_DIR/database ~/$CONFIG_DIR/blocks ~/$CONFIG_DIR/chainstate && sleep 1
     sudo rm -rf .zelbenchmark && sleep 1
-    rm -rf $BOOTSTRAP_ZIPFILE && sleep 1
+    ## rm -rf $BOOTSTRAP_ZIPFILE && sleep 1
     rm $UPDATE_FILE > /dev/null 2>&1
     rm restart_zelflux.sh > /dev/null 2>&1
     rm zelnodeupdate.sh > /dev/null 2>&1
@@ -274,8 +274,8 @@ echo -e "${YELLOW}Cleaning...${NC}"
 rm -rf ~/$CONFIG_DIR/blocks ~/$CONFIG_DIR/chainstate	
 fi 
 
-LOCAL="/home/$USER/$BOOTSTRAP_ZIP"
-if [ -f "$LOCAL" ]
+BOOTFILE="/home/$USER/$BOOTSTRAP_ZIP"
+if [ -f "$BOOTFILE" ]
 then
 echo -e "${YELLOW}Installing wallet bootstrap please be patient...${NC}"
 unzip $BOOTSTRAP_ZIPFILE -d ~/$CONFIG_DIR
