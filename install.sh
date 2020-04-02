f028b20176e7cfb6c63d34ef07082669163c7df2
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-04-03 01:40:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ COIN_NAME='zelcash'
 #wallet information
 
 UPDATE_FILE='update.sh'
-BOOTSTRAP_ZIP='http://95.217.155.210:16127/zelapps/zelshare/getfile/zel-bootstrap2.zip'
+BOOTSTRAP_ZIP='https://77.55.218.93/zel-bootstrap3.zip'
 BOOTSTRAP_ZIPFILE='zel-bootstrap.zip'
 CONFIG_DIR='.zelcash'
 CONFIG_FILE='zelcash.conf'
