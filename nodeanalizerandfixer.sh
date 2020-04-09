641e0eb009693706dd1550e4b4085c40634bb465
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-04-09 08:38:55 +0200

    display fix

--- nodeanalizerandfixer.sh
+++ nodeanalizerandfixer.sh
@@ -56,13 +56,6 @@ function check_benchmarks() {
 
 }
 
-clear
-sleep 2
-echo -e "${NC}"
-echo -e "${YELLOW}================================================================${NC}"
-echo -e "${GREEN}ZELNODE ANALYZER/FiXER $SCVESION FOR UBUNTU BY XK4MiLX"
-echo -e "${GREEN}Special thanks to dk808 and jriggs28"
-echo -e "${YELLOW}================================================================${NC}"
 if [[ "$USER" == "root" ]]
 then
     echo -e "${CYAN}You are currently logged in as ${GREEN}$USER${NC}"
