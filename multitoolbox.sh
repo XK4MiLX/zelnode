681d6cce48a0e181438ceb4459741705a77930d5
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-04-11 10:54:25 +0200

    Update multitoolbox.sh

--- multitoolbox.sh
+++ multitoolbox.sh
@@ -307,20 +307,33 @@ echo -e "${NC}"
 echo -e "${YELLOW}Update and upgrade system...${NC}"
 apt update && apt upgrade -y
 echo -e "${YELLOW}Installing docker...${NC}"
+    
+if [[ $(lsb_release -d) = *Debian* ]]
+then
+sudo apt update
+sudo apt upgrade
+sudo apt install apt-transport-https ca-certificates curl software-properties-common gnupg2 -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt update -y
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+else
 sudo apt-get update
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common -y
+    software-properties-common -y 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-   sudo apt-get update
-   sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+fi
 
 # echo -e "${YELLOW}Creating docker group..${NC}"
 # groupadd docker
