eda157ff49933b7dadeed7e921c355b1eec30585
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-04-11 08:43:13 +0200

    Update watchdog.js

--- watchdog/watchdog.js
+++ watchdog/watchdog.js
@@ -1,5 +1,9 @@
 var shell = require('shelljs');
+var sleep = require('sleep');
+sleep.sleep(10);
 
+console.log('Watchdog v1.7 by XK4MiLX Starting...');
+console.log('=================================================================');
 function zeldaemon_check() {
 
   date = new Date();
@@ -7,26 +11,25 @@ function zeldaemon_check() {
   replace(/T/, ' ').
   replace(/\..+/, '');
 
-console.log('Watchdog v1.5 by XK4MiLX');
-console.log('Start: '+data_time );
-console.log('=========================================================');
+console.log('Summary Raport / Time: '+data_time );
+console.log('=================================================================');
 var zelbench_status = shell.exec("zelbench-cli getstatus | jq '.benchmarking'",{ silent: true }).stdout;
 var zelback_status = shell.exec("zelbench-cli getstatus | jq '.zelback'",{ silent: true }).stdout;
 var zelcash_check = shell.exec("zelcash-cli getinfo | jq '.version'",{ silent: true }).stdout;
 var zelcash_node_status = shell.exec("zelcash-cli getzelnodestatus | jq '.status'",{ silent: true }).stdout;
 if (zelcash_node_status == ""){
 } else{
-console.log('Zelnode status = '+zelcash_node_status);
+console.log('Zelnode status = '+zelcash_node_status.trim());
 }
 if (zelback_status == ""){
 console.log('Zelback status = dead');
 } else{
-console.log('Zelback status = '+zelback_status);
+console.log('Zelback status = '+zelback_status.trim());
 }
 if (zelbench_status == ""){
 console.log('Zelbench status = dead');
 } else{
-console.log('Zelbench status = '+zelbench_status);
+console.log('Zelbench status = '+zelbench_status.trim());
 }
 if (zelcash_check !== "" ){
 console.log('Zelcash status =  "running"');
@@ -42,6 +45,6 @@ if ( zelbench_status.trim() == '"toaster"')
 shell.exec("zelbench-cli restartnodebenchmarks",{ silent: true });
 console.log('Zelbench restarting...');
 }
-console.log('=========================================================');
+console.log('=================================================================');
 }
 setInterval(zeldaemon_check, 150000);
