2c81db3a4f6626e5d24621b5f23d9255b79a8c7c
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-04-14 12:06:46 +0200

    Update multitoolbox.sh

--- multitoolbox.sh
+++ multitoolbox.sh
@@ -180,11 +180,11 @@ if ! pm2 -v > /dev/null 2>&1; then
     pm2 start ~/zelflux/start.sh --name zelflux
     pm2 save
     pm2 install pm2-logrotate
-    pm2 set pm2-logrotate:max_size 6M >/dev/null
-    pm2 set pm2-logrotate:retain 6 >/dev/null
-    pm2 set pm2-logrotate:compress true >/dev/null
-    pm2 set pm2-logrotate:workerInterval 3600 >/dev/null
-    pm2 set pm2-logrotate:rotateInterval 0 12 * * 0 >/dev/null 
+    pm2 set pm2-logrotate:max_size 6M > /dev/null
+    pm2 set pm2-logrotate:retain 6 > /dev/null
+    pm2 set pm2-logrotate:compress true > /dev/null
+    pm2 set pm2-logrotate:workerInterval 3600 > /dev/null
+    pm2 set pm2-logrotate:rotateInterval 0 12 * * 0 > /dev/null 
     source ~/.bashrc
     
     
