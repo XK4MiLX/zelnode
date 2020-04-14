289c2079cf23da8ec0dee93ff97e76596f0b7ce9
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-04-14 15:54:02 +0200

    Update nodeanalizerandfixer.sh

--- nodeanalizerandfixer.sh
+++ nodeanalizerandfixer.sh
@@ -201,9 +201,7 @@ echo -e ""
 
 if [[ ! whiptail --yesno "Detected IP address is $WANIP is this correct?" 8 60 ]]
 then
-
-export NEWT_COLORS='
-'
+export NEWT_COLORS=''
    WANIP=$(whiptail  --title "ZelNode ANALIZER/FiXER $SCVESION" --inputbox "        Enter IP address" 8 36 3>&1 1>&2 2>&3)
 fi
 
