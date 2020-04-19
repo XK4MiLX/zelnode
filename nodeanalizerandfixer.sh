e05a809c2ca2b1d849b0ca0efaf5eb501a428188
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-04-19 15:51:11 +0200

    Update nodeanalizerandfixer.sh

--- nodeanalizerandfixer.sh
+++ nodeanalizerandfixer.sh
@@ -337,26 +337,26 @@ echo -e "${PIN} ${CYAN}Collateral: ${SEA}$collateral${NC}"
 echo -e ""
 echo "error check..."
 
-txhash=$(grep -o '\w*' | sed -n '2p' | egrep '\w{10,50}' <<< "$collateral")
+txhash=$(grep -o '\w*' <<< "$collateral")
 
 echo "txhash: $txhash"
 
-if [[ "$txhash" != "" ]]; then
+#if [[ "$txhash" != "" ]]; then
 
-url_to_check="https://explorer.zel.cash/api/tx/$txhash"
-conf=$(wget -nv -qO - $url_to_check | jq '.confirmations')
+#url_to_check="https://explorer.zel.cash/api/tx/$txhash"
+#conf=$(wget -nv -qO - $url_to_check | jq '.confirmations')
 
-if [[ $conf == ?(-)+([0-9]) ]]; then
-if [ "$conf" -ge "100" ]; then
-echo -e "${CHECK_MARK} ${CYAN}Confirmations numbers >= 100($conf)${NC}"
-else
-echo -e "${X_MARK} ${CYAN}Confirmations numbers < 100($conf)${NC}"
-fi
-else
-echo -e "${X_MARK} ${CYAN}Zelnodeoutpoint is not valid or explorer.zel.cash is unavailable${NC}"
-fi
+#if [[ $conf == ?(-)+([0-9]) ]]; then
+#if [ "$conf" -ge "100" ]; then
+#echo -e "${CHECK_MARK} ${CYAN}Confirmations numbers >= 100($conf)${NC}"
+#else
+#echo -e "${X_MARK} ${CYAN}Confirmations numbers < 100($conf)${NC}"
+#fi
+#else
+#echo -e "${X_MARK} ${CYAN}Zelnodeoutpoint is not valid or explorer.zel.cash is unavailable${NC}"
+#fi
 
-fi
+#fi
 
 fi
 
