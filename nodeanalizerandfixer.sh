0140322c10ac12f01c6ad7cdcac057d304ba51db
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-04-29 11:25:03 +0200

    Update nodeanalizerandfixer.sh

--- nodeanalizerandfixer.sh
+++ nodeanalizerandfixer.sh
@@ -553,7 +553,7 @@ if [[ "$required_ver" != "" ]]; then
    if [ "$(printf '%s\n' "$required_ver" "$current_ver" | sort -V | head -n1)" = "$required_ver" ]; then 
       echo -e "${CHECK_MARK} ${CYAN} You have the current version of Zelflux ${GREEN}(v$required_ver)${NC}"     
    else
-      echo -e "${HOT} ${CYAN} New version of Zelflux available ${SEA}$required_ver${NC}"
+      echo -e "${HOT} ${CYAN}New version of Zelflux available ${SEA}$required_ver${NC}"
       FLUX_UPDATE="1"
    fi
  fi
