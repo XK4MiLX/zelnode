ee4294b965699c0f47d02ab215cc57a13d1a7621
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-04-29 12:28:03 +0200

    Update nodeanalizerandfixer.sh

--- nodeanalizerandfixer.sh
+++ nodeanalizerandfixer.sh
@@ -643,3 +643,46 @@ fi
 fi
 fi
 
+if [[ "$REPLACE" == "1" ]]; then
+read -p "Would you like to correct zelcash.conf errors Y/N?" -n 1 -r
+echo -e ""
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+echo -e "${YELLOW}Stopping Zelcash serivce...${NC}"
+sudo systemctl stop zelcash
+sudo fuser -k 16125/tcp > /dev/null 2>&1
+echo -e ""
+
+if [[ "zelnodeprivkey=$zelnodeprivkey" == $(grep -w zelnodeprivkey ~/.zelcash/zelcash.conf) ]]; then
+echo -e "\c"
+        else
+        sed -i "s/$(grep -e zelnodeprivkey ~/.zelcash/zelcash.conf)/zelnodeprivkey=$zelnodeprivkey/" ~/.zelcash/zelcash.conf
+                if [[ "zelnodeprivkey=$zelnodeprivkey" == $(grep -w zelnodeprivkey ~/.zelcash/zelcash.conf) ]]; then
+                        echo -e " ${CYAN}Zelnodeprivkey replaced successful...............[${CHECK_MARK}${CYAN}]${NC}"
+                fi
+fi
+if [[ "zelnodeoutpoint=$zelnodeoutpoint" == $(grep -w zelnodeoutpoint ~/.zelcash/zelcash.conf) ]]; then
+echo -e "\c"
+        else
+        sed -i "s/$(grep -e zelnodeoutpoint ~/.zelcash/zelcash.conf)/zelnodeoutpoint=$zelnodeoutpoint/" ~/.zelcash/zelcash.conf
+                if [[ "zelnodeoutpoint=$zelnodeoutpoint" == $(grep -w zelnodeoutpoint ~/.zelcash/zelcash.conf) ]]; then
+                        echo -e " ${CYAN}Zelnodeoutpoint replaced successful...............[${CHECK_MARK}${CYAN}]${NC}"
+                fi
+fi
+if [[ "zelnodeindex=$zelnodeindex" == $(grep -w zelnodeindex ~/.zelcash/zelcash.conf) ]]; then
+echo -e "\c"
+        else
+        sed -i "s/$(grep -w zelnodeindex ~/.zelcash/zelcash.conf)/zelnodeindex=$zelnodeindex/" ~/.zelcash/zelcash.conf
+                if [[ "zelnodeindex=$zelnodeindex" == $(grep -w zelnodeindex ~/.zelcash/zelcash.conf) ]]; then
+                        echo -e " ${CYAN}Zelnodeindex replaced successful...............[${CHECK_MARK}${CYAN}]${NC}"
+                fi
+fi
+echo -e ""
+sudo systemctl start zelcash
+NUM='35'
+MSG1=' Restarting zelcash serivce...'
+MSG2="${CYAN}............[${CHECK_MARK}${CYAN}]${NC}"
+spinning_timer
+echo -e ""
+fi
+fi
+
