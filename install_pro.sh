ea6a5d23359f11424088650e832c6282d1108833
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-05-04 07:22:39 +0200

    Update install_pro.sh

--- install_pro.sh
+++ install_pro.sh
@@ -1211,7 +1211,7 @@ echo -e "${ARROW} ${YELLOW}Nodejs installing...${NC}"
  # git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)` > /dev/null 2>&1
 #) && \. "$NVM_DIR/nvm.sh"
 #cd
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl --silent -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash > /dev/null 2>&1
 . ~/.profile
 source ~/.bashrc
 sleep 1
