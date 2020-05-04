0170a6ef4a095e1ed7befe67e9c4e1ed74bcc957
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-05-04 14:20:32 +0200

    Update multitoolbox.sh

--- multitoolbox.sh
+++ multitoolbox.sh
@@ -6,19 +6,25 @@ CONFIG_DIR='.zelcash'
 CONFIG_FILE='zelcash.conf'
 
 #color codes
-PINK='\033[1;95m'
 RED='\033[1;31m'
 YELLOW='\033[1;33m'
 BLUE="\\033[38;5;27m"
+SEA="\\033[38;5;49m"
 GREEN='\033[1;32m'
-NC='\033[0m'
 CYAN='\033[1;36m'
+NC='\033[0m'
+
 #emoji codes
 CHECK_MARK="${GREEN}\xE2\x9C\x94${NC}"
-X_MARK="${RED}\xE2\x9D\x8C${NC}"
+X_MARK="${RED}\xE2\x9C\x96${NC}"
 PIN="${RED}\xF0\x9F\x93\x8C${NC}"
+CLOCK="${GREEN}\xE2\x8C\x9B${NC}"
+ARROW="${SEA}\xE2\x96\xB6${NC}"
+BOOK="${RED}\xF0\x9F\x93\x8B${NC}"
+HOT="${ORANGE}\xF0\x9F\x94\xA5${NC}"
 WORNING="${RED}\xF0\x9F\x9A\xA8${NC}"
 dversion="v3.7"
+
 #dialog color
 export NEWT_COLORS='
 title=black,
