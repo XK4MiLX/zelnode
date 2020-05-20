5eecf7ff48440df07c2467b0104bb8494bacdc09
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-05-20 20:38:32 +0200

    Update helper.sh

--- helper.sh
+++ helper.sh
@@ -41,34 +41,34 @@ echo -e "${ARROW} ${CYAN}Install package for: ${GREEN}$1${NC}"
 
 sudo apt-get purge "$1" -y >/dev/null 2>&1 && sleep 1
 sudo rm /etc/apt/sources.list.d/zelcash.list >/dev/null 2>&1 && sleep 1
-echo -e "${ARROW} ${CYAN}Adding apt sources...{NC}"
+echo -e "${ARROW} ${CYAN}Adding apt sources...${NC}"
 echo 'deb https://apt.zel.cash/ all main' | sudo tee /etc/apt/sources.list.d/zelcash.list
-gpg --keyserver keyserver.ubuntu.com --recv 4B69CA27A986265D
-gpg --export 4B69CA27A986265D | sudo apt-key add -
+gpg --keyserver keyserver.ubuntu.com --recv 4B69CA27A986265D >/dev/null 2>&1
+gpg --export 4B69CA27A986265D | sudo apt-key add - >/dev/null 2>&1
 sudo apt-get update >/dev/null 2>&1
 sudo apt-get install "$1" -y >/dev/null 2>&1
 sudo chmod 755 "$COIN_PATH/$1"* && sleep 2
 if ! gpg --list-keys Zel >/dev/null; then
-  gpg --keyserver na.pool.sks-keyservers.net --recv 4B69CA27A986265D
-  gpg --export 4B69CA27A986265D | sudo apt-key add -
-  sudo apt-get update >/dev/null 2>&1
+  gpg --keyserver na.pool.sks-keyservers.net --recv 4B69CA27A986265D >/dev/null 2>&1
+  gpg --export 4B69CA27A986265D | sudo apt-key add - >/dev/null 2>&1
+  sudo apt-get update >/dev/null 2>&1 
   sudo apt-get install "$1" -y >/dev/null 2>&1
   sudo chmod 755 "$COIN_PATH/$1"* && sleep 2
   if ! gpg --list-keys Zel >/dev/null; then
-    gpg --keyserver eu.pool.sks-keyservers.net --recv 4B69CA27A986265D
-    gpg --export 4B69CA27A986265D | sudo apt-key add -
+    gpg --keyserver eu.pool.sks-keyservers.net --recv 4B69CA27A986265D >/dev/null 2>&1
+    gpg --export 4B69CA27A986265D | sudo apt-key add - >/dev/null 2>&1
     sudo apt-get update >/dev/null 2>&1
     sudo apt-get install "$1" -y >/dev/null 2>&1
     sudo chmod 755 "$COIN_PATH/$1"* && sleep 2
     if ! gpg --list-keys Zel >/dev/null; then
-      gpg --keyserver pgpkeys.urown.net --recv 4B69CA27A986265D
-      gpg --export 4B69CA27A986265D | sudo apt-key add -
+      gpg --keyserver pgpkeys.urown.net --recv 4B69CA27A986265D >/dev/null 2>&1
+      gpg --export 4B69CA27A986265D | sudo apt-key add - >/dev/null 2>&1
       sudo apt-get update >/dev/null 2>&1
       sudo apt-get install "$1" -y >/dev/null 2>&1
       sudo chmod 755 "$COIN_PATH/$1"* && sleep 2
       if ! gpg --list-keys Zel >/dev/null; then
-        gpg --keyserver keys.gnupg.net --recv 4B69CA27A986265D
-        gpg --export 4B69CA27A986265D | sudo apt-key add -
+        gpg --keyserver keys.gnupg.net --recv 4B69CA27A986265D >/dev/null 2>&1  
+        gpg --export 4B69CA27A986265D | sudo apt-key add - >/dev/null 2>&1
         sudo apt-get update >/dev/null 2>&1
         sudo apt-get install "$1" -y >/dev/null 2>&1
         sudo chmod 755 "$COIN_PATH/$1"* && sleep 2
