a944e15d920f287614e66b02c2a5f2893b6713cf
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-07-29 12:36:07 +0200

    Update helper.sh

--- helper.sh
+++ helper.sh
@@ -631,7 +631,7 @@ wget $BOOTSTRAP_URL_MONGOD -q --show-progress
 echo -e "${ARROW} ${CYAN}Unpacking...${NC}"
 tar xvf $BOOTSTRAP_ZIPFILE_MONGOD -C /home/$USER > /dev/null 2>&1 && sleep 1
 echo -e "${ARROW} ${CYAN}Importing mongodb datatable...${NC}"
-mongorestore --port 27017 --db zelcashdata /home/$USER/dump/zelcashdata --drop > /dev/null 2>&1
+mongorestore --port 27017 --db zelcashdata /home/$USER/dump/zelcashdata --drop
 echo -e "${ARROW} ${CYAN}Cleaning...${NC}"
 sudo rm -rf /home/$USER/dump > /dev/null 2>&1 && sleep 1
 sudo rm -rf $BOOTSTRAP_ZIPFILE_MONGOD > /dev/null 2>&1  && sleep 1
