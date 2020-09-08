0df437dbb456f7913ef463dc13f39e28b8a9cc31
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-09-08 12:07:15 +0200

    Update install_pro.sh

--- install_pro.sh
+++ install_pro.sh
@@ -380,7 +380,7 @@ MSG1='Zelflux loading...'
 MSG2="${CYAN}......................[${CHECK_MARK}${CYAN}]${NC}"
 spinning_timer
 echo
-DB_HIGHT=656900
+DB_HIGHT=681816
 BLOCKHIGHT=$(curl -s -m 3 http://"$WANIP":16127/explorer/scannedheight | jq '.data.generalScannedHeight')
 
 if [[ "$BLOCKHIGHT" == "null" ]]; then
