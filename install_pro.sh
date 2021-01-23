58ffa2616a093ae6453cf069611e56ef890a17a6
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-01-23 08:09:07 +0100

    Update install_pro.sh

--- install_pro.sh
+++ install_pro.sh
@@ -380,7 +380,7 @@ function mongodb_bootstrap(){
 #MSG2="${CYAN}......................[${CHECK_MARK}${CYAN}]${NC}"
 #spinning_timer
 #echo
-DB_HIGHT=681816
+DB_HIGHT=779637
 BLOCKHIGHT=$(curl -s -m 3 http://"$WANIP":16127/explorer/scannedheight | jq '.data.generalScannedHeight')
 
 if [[ "$BLOCKHIGHT" == "null" ]]; then
