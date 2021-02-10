2ad32841541f3ee5116b1900dffc48f3b2cbcf70
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-02-10 09:10:24 +0100

    Update multitoolbox.sh

--- multitoolbox.sh
+++ multitoolbox.sh
@@ -147,7 +147,7 @@ use_old_chain=$(cat /home/$USER/install_conf.json | jq -r '.use_old_chain')
 prvkey=$(cat /home/$USER/install_conf.json | jq -r '.prvkey')
 outpoint=$(cat /home/$USER/install_conf.json | jq -r '.outpoint')
 index=$(cat /home/$USER/install_conf.json | jq -r '.index')
-ZELID=$(cat /home/$USER/install_conf.json | jq -r '.zelid')
+zelid=$(cat /home/$USER/install_conf.json | jq -r '.zelid')
 
 echo -e "${ARROW} ${YELLOW}Install config summary:"
 
