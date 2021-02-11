0160c6e234958bf0af02882fe756bceddaf95c27
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-02-11 19:24:12 +0100

    Update install_pro.sh

--- install_pro.sh
+++ install_pro.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Bootstrap settings
-BOOTSTRAP_ZIP='https://fluxnodeservice.com/zel-bootstrap.zip'
+BOOTSTRAP_ZIP='https://fluxnodeservice.com/daemon_bootstrap.zip'
 BOOTSTRAP_ZIPFILE='zel-bootstrap.zip'
 BOOTSTRAP_URL_MONGOD='https://fluxnodeservice.com/mongod_bootstrap.tar.gz'
 BOOTSTRAP_ZIPFILE_MONGOD='mongod_bootstrap.tar.gz'
