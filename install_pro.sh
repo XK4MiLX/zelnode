0c58faaa83e5d9aa19fe48228cb13b75c4198d6f
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-02-11 23:27:56 +0100

    Update install_pro.sh

--- install_pro.sh
+++ install_pro.sh
@@ -306,7 +306,7 @@ echo -e "${ARROW} ${YELLOW}Restore mongodb datatable from bootstrap${NC}"
 #MSG2="${CYAN}......................[${CHECK_MARK}${CYAN}]${NC}"
 #spinning_timer
 #echo
-DB_HIGHT=$(curl -s -m 3 https://fluxnodeservice.com/mongodb-bootstrap.json | jq -r '.blocks_height')
+DB_HIGHT=$(curl -s -m 3 https://fluxnodeservice.com/mongodb_bootstrap.json | jq -r '.blocks_height')
 BLOCKHIGHT=$(curl -s -m 3 http://"$WANIP":16127/explorer/scannedheight | jq '.data.generalScannedHeight')
 
 if [[ "$BLOCKHIGHT" == "null" ]]; then
