09f1e3918b7b7e79689f9ab88e59e188e0f48bb0
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-02-12 11:37:17 +0100

    Update install_pro.sh

--- install_pro.sh
+++ install_pro.sh
@@ -1063,7 +1063,7 @@ function start_daemon() {
 }
 
 function log_rotate() {
-    echo -e "${ARROW} ${YELLOW}Configuring log rotate function for $1 debug logs...${NC}"
+    echo -e "${ARROW} ${YELLOW}Configuring log rotate function for $1 logs...${NC}"
     sleep 1
     if [ -f /etc/logrotate.d/$2 ]; then
         sudo rm -rf /etc/logrotate.d/$2
@@ -1626,8 +1626,8 @@ fi
     create_service
     install_process
     start_daemon
-    log_rotate "Benchmark" "bench_debug_log" "/home/$USER/$BENCH_DIR_LOG/debug.log" "monthly" "2"
-    log_rotate "Daemon" "daemon_debug_log" "/home/$USER/$CONFIG_DIR/debug.log" "daily" "7"
+    log_rotate "Flux benchmark" "bench_debug_log" "/home/$USER/$BENCH_DIR_LOG/debug.log" "monthly" "2"
+    log_rotate "Flux daemon" "daemon_debug_log" "/home/$USER/$CONFIG_DIR/debug.log" "daily" "7"
     log_rotate "MongoDB" "mongod_debug_log" "/var/log/mongodb/*.log" "daily" "14"
     basic_security
     status_loop
