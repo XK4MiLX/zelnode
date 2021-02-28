d4bfcb615bdfe0f45c95de3107d84bf1a294e9d1
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-02-28 12:36:35 +0100

    added Kadena address validation

--- install_pro.sh
+++ install_pro.sh
@@ -1364,7 +1364,20 @@ fi
         done
 	
            if whiptail --yesno "Are you planning to run Kadena node? Please note that only Super/BAMF nodes are allowed to run it." 8 60 3>&1 1>&2 2>&3; then
-              KDA_A=$(whiptail --inputbox "Please enter your Kadena address from Zelcore. Copy and paste the first address under the QR code. Do not edit out anything just paste what you copied." 8 85 3>&1 1>&2 2>&3)
+	   
+	    while true
+                do
+		
+                    KDA_A=$(whiptail --inputbox "Please enter your Kadena address from Zelcore. Copy and paste the first address under the QR code. Do not edit out anything just paste what you copied." 8 85 3>&1 1>&2 2>&3)
+                    if [[ "$KDA_A" = *kadena* &&  "$KDA_A" = *chainid* ]]; then
+                        echo -e "${ARROW} ${CYAN}Kadena address is valid${CYAN}.........................[${CHECK_MARK}${CYAN}]${NC}"
+                        break
+                    else
+                        echo -e "${ARROW} ${CYAN}Kadena address is not valid try again...........[${X_MARK}${CYAN}]${NC}}"
+                        sleep 4
+                    fi
+            done
+	                 
            fi
 	
         fi
