16662d8f418897bade1e23b1969cda98f1085715
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-03-03 14:11:07 +0100

    Update nodeanalizerandfixer.sh

--- nodeanalizerandfixer.sh
+++ nodeanalizerandfixer.sh
@@ -702,12 +702,24 @@ pm2_flux_status=$(pm2 info flux 2> /dev/null | grep 'status' | sed -r 's/│//gi
 if [[ "$pm2_flux_status" == "online" ]]; then
 pm2_flux_uptime=$(pm2 info flux | grep 'uptime' | sed -r 's/│//gi' | sed 's/uptime//g' | xargs)
 pm2_flux_restarts=$(pm2 info flux | grep 'restarts' | sed -r 's/│//gi' | xargs)
-echo -e "${CHECK_MARK} ${CYAN} Pm2 Flux info => status: ${GREEN}$pm2_flux_status${CYAN}, uptime: ${GREEN}$pm2_flux_uptime${NC} ${SEA}$pm2_flux_restarts${NC}" 
+echo -e "${CHECK_MARK} ${CYAN} Pm2 Flux info => status: ${GREEN}$pm2_flux_status${CYAN}, uptime: ${GREEN}$pm2_flux_uptime${NC} ${SEA}$pm2_flux_restarts${NC}"
 else
 if [[ "$pm2_flux_status" != "" ]]; then
 echo -e "${X_MARK} ${CYAN} Pm2 Flux status: ${RED}$pm2_flux_status ${NC}" 
 fi
 fi
+
+pm2_flux_status=$(pm2 info zelflux 2> /dev/null | grep 'status' | sed -r 's/│//gi' | sed 's/status.//g' | xargs)
+if [[ "$pm2_flux_status" == "online" ]]; then
+pm2_flux_uptime=$(pm2 info zelflux | grep 'uptime' | sed -r 's/│//gi' | sed 's/uptime//g' | xargs)
+pm2_flux_restarts=$(pm2 info zelflux | grep 'restarts' | sed -r 's/│//gi' | xargs)
+echo -e "${CHECK_MARK} ${CYAN} Pm2 Flux info => status: ${GREEN}$pm2_flux_status${CYAN}, uptime: ${GREEN}$pm2_flux_uptime${NC} ${SEA}$pm2_flux_restarts${NC}"
+else
+if [[ "$pm2_flux_status" != "" ]]; then
+echo -e "${X_MARK} ${CYAN} Pm2 Flux status: ${RED}$pm2_flux_status ${NC}" 
+fi
+fi
+
 else
 echo -e "${X_MARK} ${CYAN} Pm2 is not installed${NC}"
 if tmux ls | grep created &> /dev/null; then
