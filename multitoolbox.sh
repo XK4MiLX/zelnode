b127c7c73898a7bc7a8ec8c7bb337bde942988a8
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-03-05 01:29:14 +0100

    Update multitoolbox.sh

--- multitoolbox.sh
+++ multitoolbox.sh
@@ -1147,10 +1147,7 @@ sudo apt-get install \
     gnupg-agent \
     software-properties-common -y 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
-sudo add-apt-repository \
-   "deb [arch=amd64,arm64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+sudo add-apt-repository -y "deb [arch=amd64,arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update -y 
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y 
 
