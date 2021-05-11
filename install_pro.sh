01b6f629dfd7f9fe9733685b79ba404423fde1d6
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-05-11 23:28:12 +0200

    increased daemon loading time

--- install_pro.sh
+++ install_pro.sh
@@ -1342,7 +1342,7 @@ function start_daemon() {
     sudo systemctl enable zelcash.service > /dev/null 2>&1
     sudo systemctl start zelcash > /dev/null 2>&1
     
-    NUM='180'
+    NUM='210'
     MSG1='Starting daemon & syncing with chain please be patient this will take about 3 min...'
     MSG2=''
     spinning_timer
