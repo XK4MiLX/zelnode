89b9f8026e54f9acaa07551364de7b8f96f1eaf5
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-05-30 12:30:10 +0200

    added flux mark

--- multitoolbox.sh
+++ multitoolbox.sh
@@ -288,7 +288,7 @@ fi
  pm2 save > /dev/null 2>&1
  fi
  
-docker_check=$(docker container ls -a | grep 'zelcash' | grep -Eo "^[0-9a-z]{8,}\b" | wc -l)
+docker_check=$(docker container ls -a | egrep 'zelcash|flux' | grep -Eo "^[0-9a-z]{8,}\b" | wc -l)
 resource_check=$(df | egrep 'flux' | awk '{ print $1}' | wc -l)
 mongod_check=$(mongoexport -d localzelapps -c zelappsinformation --jsonArray --pretty --quiet  | jq -r .[].name | head -n1)
 
@@ -304,7 +304,7 @@ echo -e "${ARROW} ${CYAN}Removing containers...${NC}"
 sudo aa-remove-unknown && sudo service docker restart > /dev/null 2>&1 && sleep 2 
 sleep 5
 #docker ps | grep -Eo "^[0-9a-z]{8,}\b" |
-docker container ls -a | grep 'zelcash' | grep -Eo "^[0-9a-z]{8,}\b" |
+docker container ls -a | egrep 'zelcash|flux' | grep -Eo "^[0-9a-z]{8,}\b" |
 while read line; do
 sudo docker stop $line > /dev/null 2>&1 && sleep 2 
 sudo docker rm $line > /dev/null 2>&1 && sleep 2 
