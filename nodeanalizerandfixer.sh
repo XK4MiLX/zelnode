64923a0a69cfb887227dc63cbef0cfaeb933b191
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-11-14 19:59:44 +0100

    Minor edit

--- nodeanalizerandfixer.sh
+++ nodeanalizerandfixer.sh
@@ -866,8 +866,9 @@ if [[ "$required_ver" != "" ]]; then
  fi
 
 echo -e "${CHECK_MARK} ${CYAN} Flux config  ~/$FLUX_DIR/config/userconfig.js exists${NC}"
-ZELIDLG=`echo -n $(grep -w zelid /home/$USER/$FLUX_DIR/config/userconfig.js | sed -e 's/.*zelid: .//') | wc -m`
-if [ "$ZELIDLG" -eq "36" ] || [ "$ZELIDLG" -eq "35" ]; then
+
+ZELIDLG=`echo -n $(grep -w zelid /home/$USER/$FLUX_DIR/config/userconfig.js | sed -e "s/'//g" | sed -e "s/,//g" | sed -e "s/.*zelid://g") | wc -m`
+if [ "$ZELIDLG" -eq "35" ] || [ "$ZELIDLG" -eq "34" ]; then
 echo -e "${CHECK_MARK} ${CYAN} Zel ID is valid${NC}"
 elif [[ "$ZELIDLG" == "0" || "$ZELIDLG" == "2" ]]; then
 echo -e "${X_MARK} ${CYAN} Zel ID is missing...${NC}"
