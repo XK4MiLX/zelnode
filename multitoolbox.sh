a54bbd32a0725d2c66fbb6ed57d07a69359099e0
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-12-28 09:42:14 +0100

    disable error output

--- multitoolbox.sh
+++ multitoolbox.sh
@@ -1826,7 +1826,7 @@ apps_id=$(docker run -d \
 --name watchtower \
 -v /var/run/docker.sock:/var/run/docker.sock \
 containrrr/watchtower \
---label-enable --cleanup --interval $random)
+--label-enable --cleanup --interval $random) 2> /dev/null
 if [[ $apps_id =~ ^[[:alnum:]]+$ ]]; then
 echo -e "${ARROW} ${CYAN}Watchtower installed successful, id: ${GREEN}$apps_id${NC}"
 else
