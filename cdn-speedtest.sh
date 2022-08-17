1d7074cdc1ff0a7281b9f2f7fd77b6a4d1eeb127
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2022-08-17 16:49:31 +0200

    added Average speed

--- cdn-speedtest.sh
+++ cdn-speedtest.sh
@@ -30,7 +30,8 @@ while [ $i -lt $len ];
 do
     testing=$(curl -m 4 http://cdn-${rand_by_domain[$i]}.runonflux.io/apps/fluxshare/getfile/flux_explorer_bootstrap.tar.gz  --output testspeed -fail --silent --show-error 2>&1)
     testing_size=$(grep -Po "\d+" <<< "$testing" | paste - - - - | awk '{printf  "%d\n",$3}')
-    echo -e "  ${PIN} ${GREEN}cdn-${YELLOW}${rand_by_domain[$i]}${GREEN} - Bits Downloaded: ${YELLOW}$testing_size${NC}"
+    mb=$(bc <<<"scale=2; $testing_size / 1048576 / 4" | awk '{printf "%2.2f\n", $1}')
+    echo -e "  ${PIN} ${GREEN}cdn-${YELLOW}${rand_by_domain[$i]}${GREEN} - Bits Downloaded: ${YELLOW}$testing_size${NC} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
     size_list+=($testing_size)
     i=$(($i+1))
 done
@@ -43,7 +44,8 @@ for i in "${!size_list[@]}"; do
 done
 
 # Print the results
+mb=$(bc <<<"scale=2; $arr_max / 1048576 / 4" | awk '{printf "%2.2f\n", $1}')
 echo -e ""
-echo -e "${YELLOW}Best server is: ${GREEN}cdn-${YELLOW}${rand_by_domain[${max_indexes[0]}]}${NC}"
+echo -e "${YELLOW}Best server is: ${GREEN}cdn-${YELLOW}${rand_by_domain[${max_indexes[0]}]} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
 echo -e "${CHECK_MARK} ${GREEN}Fastest Server: ${YELLOW}http://cdn-${rand_by_domain[${max_indexes[@]}]}.runonflux.io/apps/fluxshare/getfile/flux_explorer_bootstrap.tar.gz${NC}"
 echo -e ""
