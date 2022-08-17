213526182ba7c6c3726ef0cb326ecd8278811cf2
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2022-08-17 13:26:00 +0200

    Minor edits

--- nodeanalizerandfixer.sh
+++ nodeanalizerandfixer.sh
@@ -731,13 +731,13 @@ daemon_inactive=$(sudo systemctl status zelcash 2> /dev/null | egrep 'inactive|f
 #fi
 
   if sudo systemctl list-units | grep docker.service | egrep -wi 'running' > /dev/null 2>&1; then
-    echo -e "${ARROW}  ${CYAN}Docker service running ${SEA}$docker_running${NC}"
+    echo -e "${CHECK_MARK}  ${CYAN}Docker service running ${SEA}$docker_running${NC}"
     #docker_working=1
   else
     if [[ "$docker_inactive" != "" ]]; then
-       echo -e "${ARROW}  ${CYAN}Docker service not running ${RED}$docker_inactive${NC}"
+       echo -e "${X_MARK}  ${CYAN}Docker service not running ${RED}$docker_inactive${NC}"
     else
-    echo -e "${ARROW}  ${CYAN}Docker is not installed${NC}"
+    echo -e "${X_MARK}  ${CYAN}Docker is not installed${NC}"
   fi
 fi
 
