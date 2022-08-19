8c430c8db7047f1231f736c48f24a52783950296
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2022-08-19 12:08:33 +0200

    improvement
    - supporting custom test time via argument
    - supporting custom file_name via argument
    - supporting custom url list via argument ( array )
    - added duration ,  servers counter , valid servers counter

--- cdn-speedtest.sh
+++ cdn-speedtest.sh
@@ -1,42 +1,78 @@
 #!/bin/bash
 
 #color codes
-RED='\033[1;31m'
 YELLOW='\033[1;33m'
-BLUE="\\033[38;5;27m"
-SEA="\\033[38;5;49m"
 GREEN='\033[1;32m'
 CYAN='\033[1;36m'
 NC='\033[0m'
 
 #emoji codes
-CHECK_MARK="${GREEN}\xE2\x9C\x94${NC}"
-X_MARK="${RED}\xE2\x9C\x96${NC}"
-PIN="${RED}\xF0\x9F\x93\x8C${NC}"
-CLOCK="${GREEN}\xE2\x8C\x9B${NC}"
 ARROW="${SEA}\xE2\x96\xB6${NC}"
-BOOK="${RED}\xF0\x9F\x93\x8B${NC}"
-HOT="${ORANGE}\xF0\x9F\x94\xA5${NC}"
-WORNING="${RED}\xF0\x9F\x9A\xA8${NC}"
 RIGHT_ANGLE="${GREEN}\xE2\x88\x9F${NC}"
 
-rand_by_domain=("5" "6" "7" "8" "9" "10" "11" "12")
+#global variable
+server_offline="0"
+failed_counter"0"
+
+
+if [[ -z $1 || "$1" == "0" ]]; then
+    BOOTSTRAP_FILE="flux_explorer_bootstrap.tar.gz"
+else
+    BOOTSTRAP_FILE="$1"
+fi
+
+if [[ -z $2 ]]; then
+    dTime="5"
+else
+    dTime="$2"
+fi
+
+if [[ -z $3 ]]; then
+    rand_by_domain=("5" "6" "7" "8" "9" "10" "11" "12")
+else
+    msg="$3"
+    shift
+    shift
+    rand_by_domain=("$@")
+    custom_url="1"
+fi
+
 size_list=()
 i=0
 len=${#rand_by_domain[@]}
-echo -e ""
-echo -e "${YELLOW}Running quick download speed test for flux_explorer_bootstrap...${NC}"
+echo -e "${ARROW} ${CYAN}Running quick download speed test for ${BOOTSTRAP_FILE}, Servers: ${GREEN}$len${NC}"
+start_test=`date +%s`
 while [ $i -lt $len ];
 do
-    testing=$(curl -m 4 http://cdn-${rand_by_domain[$i]}.runonflux.io/apps/fluxshare/getfile/flux_explorer_bootstrap.tar.gz  --output testspeed -fail --silent --show-error 2>&1)
+    if [[ "$custom_url" == "1" ]]; then
+            testing=$(curl -m ${dTime} ${rand_by_domain[$i]}${BOOTSTRAP_FILE}  --output testspeed -fail --silent --show-error 2>&1)
+    else
+            testing=$(curl -m ${dTime} http://cdn-${rand_by_domain[$i]}.runonflux.io/apps/fluxshare/getfile/${BOOTSTRAP_FILE}  --output testspeed -fail --silent --show-error 2>&1)
+    fi
     testing_size=$(grep -Po "\d+" <<< "$testing" | paste - - - - | awk '{printf  "%d\n",$3}')
-    mb=$(bc <<<"scale=2; $testing_size / 1048576 / 4" | awk '{printf "%2.2f\n", $1}')
-    echo -e "   ${RIGHT_ANGLE} ${GREEN}cdn-${YELLOW}${rand_by_domain[$i]}${GREEN} - Bits Downloaded: ${YELLOW}$testing_size${NC} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
+    mb=$(bc <<<"scale=2; $testing_size / 1048576 / $dTime" | awk '{printf "%2.2f\n", $1}')
+
+    if [[ "$custom_url" == "1" ]]; then
+        domain=$(sed -e 's|^[^/]*//||' -e 's|/.*$||' <<< ${rand_by_domain[$i]})
+        echo -e "  ${RIGHT_ANGLE} ${GREEN}URL - ${YELLOW}${domain}${GREEN} - Bits Downloaded: ${YELLOW}$testing_size${NC} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
+    else
+        echo -e "  ${RIGHT_ANGLE} ${GREEN}cdn-${YELLOW}${rand_by_domain[$i]}${GREEN} - Bits Downloaded: ${YELLOW}$testing_size${NC} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
+    fi
     size_list+=($testing_size)
+    if [[ "$testing_size" == "0" ]]; then
+        failed_counter=$(($failed_counter+1))
+    fi
     i=$(($i+1))
 done
 
+rServerList=$((${#size_list[@]}-$failed_counter))
+echo -e "${ARROW} ${CYAN}Valid servers: ${GREEN}${rServerList} ${CYAN}- Duration: ${GREEN}$((($(date +%s)-$start_test)/60)) min. $((($(date +%s)-$start_test) % 60)) sec.${NC}"
 sudo rm -rf testspeed > /dev/null 2>&1
+
+if [[ "$rServerList" == "0" ]]; then
+    server_offline="1"
+    return 1
+fi
 
 arr_max=$(printf '%s\n' "${size_list[@]}" | sort -n | tail -1)
 for i in "${!size_list[@]}"; do
@@ -44,9 +80,18 @@ for i in "${!size_list[@]}"; do
     max_indexes+=($i)
 done
 
-# Print the results
-mb=$(bc <<<"scale=2; $arr_max / 1048576 / 4" | awk '{printf "%2.2f\n", $1}')
-echo -e ""
-echo -e "${YELLOW}Best server is: ${GREEN}cdn-${YELLOW}${rand_by_domain[${max_indexes[0]}]} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
-echo -e "${CHECK_MARK} ${GREEN}Fastest Server: ${YELLOW}http://cdn-${rand_by_domain[${max_indexes[@]}]}.runonflux.io/apps/fluxshare/getfile/flux_explorer_bootstrap.tar.gz${NC}"
-echo -e ""
+server_index=${rand_by_domain[${max_indexes[0]}]}
+if [[ "$custom_url" == "1" ]]; then
+    BOOTSTRAP_URL="$server_index"
+else
+    BOOTSTRAP_URL="http://cdn-${server_index}.runonflux.io/apps/fluxshare/getfile/"
+fi
+DOWNLOAD_URL="${BOOTSTRAP_URL}${BOOTSTRAP_FILE}"
+
+mb=$(bc <<<"scale=2; $arr_max / 1048576 / $dTime" | awk '{printf "%2.2f\n", $1}')
+if [[ "$custom_url" == "1" ]]; then
+    domain=$(sed -e 's|^[^/]*//||' -e 's|/.*$||' <<< ${server_index})
+    echo -e "${ARROW} ${CYAN}Best server is: ${YELLOW}${domain} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
+else
+    echo -e "${ARROW} ${CYAN}Best server is: ${GREEN}cdn-${YELLOW}${rand_by_domain[${max_indexes[0]}]} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
+fi
