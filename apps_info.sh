c772e28b324be39ca5e5bac42331ecab00181c9b
XK4MiLX <C:\Users\k4mil\AppData\Roaming\The Bat!>
2022-08-31 22:49:19 +0200

    added networks variable

--- apps_info.sh
+++ apps_info.sh
@@ -12,11 +12,14 @@ else
 	export ROOT_BRANCH='master'
 fi
 
+network_url_1="explorer.zelcash.online"
+network_url_2="explorer.runonflux.io"
+
 name=($(jq -r .data[].name <<< "$apps_info"))
 height=($(jq -r .data[].height <<< "$apps_info"))
-network_height_01=$(curl -sk -m 5 https://explorer.runonflux.io/api/status?q=getInfo | jq '.info.blocks')
-network_height_03=$(curl -sk -m 5 https://explorer.zelcash.online/api/status?q=getInfo | jq '.info.blocks')
-explorer_network_hight=$(max "$network_height_01" "$network_height_03")
+network_height_01=$(curl -sk -m 5 https://$network_url_1/api/status?q=getInfo | jq '.info.blocks')
+network_height_02=$(curl -sk -m 5 https://$network_url_2/api/status?q=getInfo | jq '.info.blocks')
+explorer_network_hight=$(max "$network_height_01" "$network_height_02")
 echo -e ""
 echo -e "Apps count: ${#name[@]}"
 echo -e "-------------------------------------"
