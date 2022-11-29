d432e2296f6d1ed13f441afec4f104155bc05620
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2022-11-29 13:45:17 +0100

    added fluxos_clean

--- flux_common.sh
+++ flux_common.sh
@@ -542,8 +542,40 @@ function config_smart_create() {
                 fi
         fi
 }
-
 ###### HELPERS SECTION
+function  fluxos_clean(){
+   docker_check=$(docker container ls -a | egrep 'zelcash|flux' | grep -Eo "^[0-9a-z]{8,}\b" | wc -l)
+   resource_check=$(df | egrep 'flux' | awk '{ print $1}' | wc -l)
+   if [[ $docker_check != 0 ]]; then
+     echo -e "${ARROW} ${CYAN}Removing containers...${NC}"
+     sudo service docker restart > /dev/null 2>&1 && sleep 2
+     docker container ls -a | egrep 'zelcash|flux' | grep -Eo "^[0-9a-z]{8,}\b" |
+     while read line; do
+       sudo docker stop $line > /dev/null 2>&1 && sleep 2
+       sudo docker rm $line > /dev/null 2>&1 && sleep 2
+     done
+   fi
+   if [[ $resource_check != 0 ]]; then
+     echo -e "${ARROW} ${CYAN}Unmounting locked FluxOS resource${NC}" && sleep 1
+     df | egrep 'flux' | awk '{ print $1}' |
+     while read line; do
+       sudo umount -l $line && sleep 1
+     done
+   fi
+   if [[ -d /home/$USER/zelflux/ZelApps ]]; then
+     echo -e "${ARROW} ${CYAN}Removing FluxApps directory...${NC}" && sleep 1
+     APPS_LIST=($(find /home/$USER/zelflux/ZelApps -maxdepth 1 -mindepth 1 -type d -printf '%P\n'))
+     LENGTH=${#APPS_LIST[@]}
+     for (( j=0; j<${LENGTH}; j++ ));
+     do
+       if [[ "${APPS_LIST[$j]}" != "ZelShare" && "${APPS_LIST[$j]}" != "" ]]; then
+         echo -e "${ARROW} ${CYAN}Apps directory removed, path: ${GREEN}/home/$USER/zelflux/ZelApps/${APPS_LIST[$j]}${NC}"
+         rm -rf /home/$USER/zelflux/ZelApps/${APPS_LIST[$j]}
+       fi
+     done
+   fi
+}
+
 function round() {
 	printf "%.${2}f" "${1}"
 }
