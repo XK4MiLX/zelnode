18a211768bcee6198ca67e157c431c1887efc901
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-07-03 11:31:32 +0200

    cleanup

--- flux_common.sh
+++ flux_common.sh
@@ -90,7 +90,6 @@ function fluxos_conf_create(){
 	  zelid: '${ZELID}',
 	  kadena: '${KDA_A}',
 	  development: false,
-	  decryptionkey: '',
 	  testnet: $testnet
 	  }
 	}
@@ -399,7 +398,7 @@ function config_builder() {
 
 function smart_reconfiguration(){
  watchdog_settings_list=("label", "tier_eps_min", "zelflux_update", "zelcash_update", "zelbench_update", "action", "ping", "web_hook_url", "telegram_alert", "telegram_bot_token", "telegram_chat_id")
- fluxos_settings_list=("kadena", "zelid", "apiport", "ipaddress", "development", "decryptionkey")
+ fluxos_settings_list=("kadena", "zelid", "apiport", "ipaddress", "development")
  daemon_settings_list=("zelnodeprivkey", "zelnodeoutpoint", "zelnodeindex")
  benchmark_settings_list=("fluxport", "thunder", "speedtestserverid")
  config_list=$(cat <<-END
@@ -417,8 +416,7 @@ function smart_reconfiguration(){
   "thunder": [{"key": "thunder", "label": "Thunder Mode"}],
   "speedtestserverid": [{"key": "speedtestserverid", "label": "Speed Test Server ID"}],
   "upnp_port": [{"key": "apiport", "label": "UPnP Port"}],
-  "development": [{"key": "development", "label": "Development Mode"}],
-  "decryptionkey": [{"key": "decryptionkey", "label": "Encryption Mode"}]
+  "development": [{"key": "development", "label": "Development Mode"}]
  }
 END
 )
