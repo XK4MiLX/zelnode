9b6fdd33871e6c5a3e1171c22bf520b53c6c182b
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-08-07 10:56:53 +0200

    cleanup

--- hardwarebench.sh
+++ hardwarebench.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 SSD=0
 HDD=0
+SCORE=0
+GREEN='\033[1;32m'
+NC='\033[0m'
 command_exists()
 {
     command -v "$@" > /dev/null 2>&1
@@ -41,22 +44,17 @@ dd_benchmark()
           }'
   sudo rm -f $1/test_*
 }
-
 if ! command_exists dd; then
   printf '%s\n' 'This script requires dd, but it could not be found.' 1>&2
   exit 1
 fi
-
-
 if [[ "$(sysbench --version)" == "" ]]; then
   curl -s https://packagecloud.io/install/repositories/akopytov/sysbench/script.deb.sh | sudo bash > /dev/null 2>&1
   sudo apt -y install sysbench  > /dev/null 2>&1
 fi
-
 vcore=$(getconf _NPROCESSORS_ONLN)
 ram=$(LC_ALL=C free -b 2> /dev/null | awk 'NR==2 {print $2}' | grep -Eo '[0-9]+'| printf "%.0f\n" $(awk '{ print $1/1024/1024/1024 }'))
 core=$(awk -F: '/cpu cores/ {name=$2} END {print name}' /proc/cpuinfo | sed 's/^[ \\t]*//;s/[ \\t]*$//')
-
 echo -e ""
 echo -e "-------------------------"
 echo -e "| MEMORY BENCHMARK"
@@ -66,38 +64,30 @@ echo -e "-------------------------"
 echo -e "| CPU BENCHMARK"
 echo -e "-------------------------"
 eps=$(LC_ALL=C sysbench cpu --cpu-max-prime=60000 --time=20 run 2> /dev/null | grep 'events per second' | awk -v cpu=$vcore '{print $4*cpu}')
-
 if [[ "$ram" -ge  7 ]] && [[ "$core" -ge  2 ]] && [[ "$vcore" -ge 4 ]] &&  [[ "${eps%%.*}" -ge 240 ]]; then
   status="CUMULUS"
 fi
-
 if [[ "$ram" -ge  31 ]] && [[ "$core" -ge  4 ]] && [[ "$vcore" -ge 8 ]] && [[ "${eps%%.*}" -ge  640 ]]; then
   status="NIMBUS"
 fi
-
 if [[ "$ram" -ge  62 ]] && [[ "$core" -ge 8 ]]  && [[ "$vcore" -ge 16 ]] && [[ "${eps%%.*}" -ge  1520 ]]; then
   status="STRATUS"
 fi
-
 if [[ "$status" == "" ]]; then
   status="FAILED"
 fi
-
 echo -e "| CPU vcores: $vcore"
 echo -e "| CPU cores: $core"
 echo -e "| EPS: $eps"
-
 outputdiskbench="Disks Bench:";
 #checking loop for lxc only if mount == '/'
 loop_mount=$(cd $HOME && LC_ALL=C lsblk -l -b -n | grep ' loop' | awk '{ if ($7 == "/") printf("%.2f\n", $4/(1024*1024*1024))}')
 if [[ "$loop_mount" != "" ]]; then
-
   #echo -e "Device type: loop"
   mount_path="/"
   io1=$( dd_benchmark "$mount_path" )
   io2=$( dd_benchmark "$mount_path" )
   io3=$( dd_benchmark "$mount_path" )
-
   if [[ "$io1" -le "$io2" ]] && [[ "$io1" -le "$io3" ]]; then
     ioavg=$( awk 'BEGIN{printf("%.0f", ('"$io2"' + '"$io3"')/2)}' | Bps_to_MiBps )
   elif [[ "$io2" -le "$io1" ]]  && [[ "$io2" -le "$io3" ]]; then
@@ -105,7 +95,6 @@ if [[ "$loop_mount" != "" ]]; then
   else
     ioavg=$( awk 'BEGIN{printf("%.0f", ('"$io1"' + '"$io2"')/2)}' | Bps_to_MiBps )
   fi
-
   outputdiskbench+=" loop $loop_mount $ioavg"
   if [[ "${ioavg%%.*}" -ge "180" ]]; then
     SSD=$(awk 'BEGIN{printf("%d",'"$SSD"'+'"$loop_mount"')}')
@@ -114,13 +103,11 @@ if [[ "$loop_mount" != "" ]]; then
   fi
 fi
 if [[ $outputdiskbench == "Disks Bench:" ]]; then
-
   lvm_mount=""
   raid_list=()
   disc__array=($(cd $HOME && LC_ALL=C lsblk -o NAME,SIZE,TYPE -b -n | grep ' disk' | awk '{ if ($2 > 2147483648 && $1 != "mmcblk0" && $1 != "mmcblk0p1") print $1}'))
   for((i=0;i<${#disc__array[@]};i++));
   do
-
     #checking if disk structure is accessable
     cd $HOME && lsblk -l -b -n /dev/${disc__array[i]} > /dev/null 2>&1
     if [ $? != 0 ]; then
@@ -147,14 +134,12 @@ if [[ $outputdiskbench == "Disks Bench:" ]]; then
           continue
         fi
       fi
-
       if [[  "$lvm_name" != "" && "$lvm_mount" == "" ]]; then
         if [[ ! -d /.benchmark_test ]]; then
           sudo mkdir /.benchmark_test
         fi
         sudo mount /dev/mapper/$lvm_name /.benchmark_test
       fi
-
       lvm_mount=$(cd $HOME && LC_ALL=C lsblk -l -b /dev/${disc__array[i]} --sort SIZE | egrep ' lvm| dm' |  tail -n1 | awk '{ print $7 }' )
       if [[ "$lvm_mount" != "" ]]; then
         partition_output=$(cd $HOME && LC_ALL=C lsblk -l -b /dev/${disc__array[i]} --sort SIZE | egrep ' lvm| dm' |  tail -n1 | awk '{print $1 " " $4/(1024*1024*1024) }')
@@ -169,7 +154,6 @@ if [[ $outputdiskbench == "Disks Bench:" ]]; then
           #echo -e "-----------------------------"
           continue
         fi
-
         #checking raid
         partition_output=$(cd $HOME && LC_ALL=C lsblk -l -b /dev/${disc__array[i]} --sort SIZE | egrep ' raid' |  tail -n1 | awk '{print $1 " " $4/(1024*1024*1024) }')
         if  [[ "$partition_output"  == "" ]]; then
@@ -192,14 +176,12 @@ if [[ $outputdiskbench == "Disks Bench:" ]]; then
           fi
         fi
       fi
-
     else
       #echo -e "Checking partition on device: /dev/${disc__array[i]} (STEP2)..."
       partition_output=$(cd $HOME && LC_ALL=C lsblk -l -b /dev/${disc__array[i]} --sort SIZE | tail -n1 | awk '{print $1 " " $4/(1024*1024*1024) }')
       #echo -e "$partition_output"
       #echo -e ""
     fi
-
     partition_name=$(awk '{print $1}' <<< $partition_output)
     partition_size=$(awk '{printf("%.2f",$2)}' <<< $partition_output)
     if [[ "$lvm_mount" == "" && "$raid_list" == "" ]]; then
@@ -207,7 +189,6 @@ if [[ $outputdiskbench == "Disks Bench:" ]]; then
     else
       disk_size=$partition_size
     fi
-
     if [[ "$lvm_mount" == "" ]]; then
       if [[ ! -d /.benchmark_test ]]; then
         sudo mkdir /.benchmark_test
@@ -219,18 +200,15 @@ if [[ $outputdiskbench == "Disks Bench:" ]]; then
     fi
 
     if [[ "$available_space" != "null" &&  "$available_space" != "" ]]; then
-
       if [[ "$lvm_mount" == "" ]]; then
         mount_path="/.benchmark_test"
       else
         mount_path="$lvm_mount"
       fi
-
       #echo -e "Mount point: $mount_path"
       io1=$( dd_benchmark "$mount_path" )
       io2=$( dd_benchmark "$mount_path" )
       io3=$( dd_benchmark "$mount_path" )
-
       if [[ "$io1" -le "$io2" ]] && [[ "$io1" -le "$io3" ]]; then
         ioavg=$( awk 'BEGIN{printf("%.0f", ('"$io2"' + '"$io3"')/2)}' | Bps_to_MiBps )
       elif [[ "$io2" -le "$io1" ]]  && [[ "$io2" -le "$io3" ]]; then
@@ -238,7 +216,6 @@ if [[ $outputdiskbench == "Disks Bench:" ]]; then
       else
         ioavg=$( awk 'BEGIN{printf("%.0f", ('"$io1"' + '"$io2"')/2)}' | Bps_to_MiBps )
       fi
-
       outputdiskbench+=" ${disc__array[i]} $disk_size $ioavg"
       if [[ "${ioavg%%.*}" -ge "180" ]]; then
         SSD=$(awk 'BEGIN{printf("%d",'"$SSD"'+'"$disk_size"')}')
@@ -254,7 +231,6 @@ if [[ $outputdiskbench == "Disks Bench:" ]]; then
           sudo rm -rf /.benchmark_test
         fi
       fi
-
       #check if test point mounted if exist unmount it LVM case
       if [[ $(cd $HOME && lsblk -l | grep "benchmark_test") ]]; then
         sudo umount /.benchmark_test
@@ -264,13 +240,10 @@ if [[ $outputdiskbench == "Disks Bench:" ]]; then
           sudo rm -rf /.benchmark_test
         fi
       fi
-
     else
-
       #echo -e "Error: space not enough... write test skipped!"
       #echo -e "-----------------------------"
       if [[ "$lvm_mount" == "" ]]; then
-
         sudo umount /.benchmark_test
         if [[ $(cd $HOME && lsblk -l | grep "benchmark_test") ]]; then
           echo -e ""
@@ -279,9 +252,7 @@ if [[ $outputdiskbench == "Disks Bench:" ]]; then
         fi
       fi
     fi
-
   done
-
   if [[ "$outputdiskbench" == "Disks Bench:" ]]; then
     # lsblk failed checking direct mount from df
     df_direct_mount=$(LC_ALL=C df --output=source,fstype,size,avail,target | grep 'dev' | awk '{ if ($5 == "/") printf("%s %.2f %.2f\n", $1,$3/(1024*1024),$4/(1024*1024))}')
@@ -299,7 +270,6 @@ if [[ $outputdiskbench == "Disks Bench:" ]]; then
       else
         ioavg=$( awk 'BEGIN{printf("%.0f", ('"$io1"' + '"$io2"')/2)}' | Bps_to_MiBps )
       fi
-
       outputdiskbench+=" $device_name $partition_size $ioavg"
       if [[ "${ioavg%%.*}" -ge "180" ]]; then
         SSD=$(awk 'BEGIN{printf("%d",'"$SSD"'+'"$partition_size"')}')
@@ -307,47 +277,34 @@ if [[ $outputdiskbench == "Disks Bench:" ]]; then
         HDD=$(awk 'BEGIN{printf("%d",'"$HDD"'+'"$partition_size"')}')
       fi
     fi
-
   fi
-
 fi
-
 echo -e "-------------------------"
 echo -e "| DISK INFO"
 echo -e "-------------------------"
 echo -e "| SSD: $SSD"
 echo -e "| HDD: $HDD"
 echo -e "-------------------------"
-
-score=0
 if [[ $status == "CUMULUS" ]]; then
-  score=1
+  SCORE=1
 fi
-
 if [[ $status == "NIMBUS" ]]; then
-  score=2
+  SCORE=2
 fi
-
 if [[ $status == "STRATUS" ]]; then
-  score=3
+  SCORE=3
 fi
-
-
-if [[ "$SSD" -lt 220 ]] || [[ "$score" == 0 ]] ; then
+if [[ "$SSD" -lt 220 ]] || [[ "$SCORE" == 0 ]] ; then
   status="FAILED"
 fi
-
-if [[ "$SSD" -ge 220 ]] && [[ "$score" -ge 1 ]]; then
-  status="CUMULUS"
+if [[ "$SSD" -ge 220 ]] && [[ "$SCORE" -ge 1 ]]; then
+  status="${GREEN}CUMULUS${NC}"
 fi
-
-if [[ "$SSD" -ge 440 ]] && [[ "$score" -ge 2 ]]; then
-  status="NIMBUS"
+if [[ "$SSD" -ge 440 ]] && [[ "$SCORE" -ge 2 ]]; then
+  status="${GREEN}NIMBUS${NC}"
 fi
-
-if [[ "$SSD" -ge 880 ]] && [[ "$score" -ge 3 ]] ; then
-  status="STRATUS"
+if [[ "$SSD" -ge 880 ]] && [[ "$SCORE" -ge 3 ]] ; then
+  status="${GREEN}STRATUS${NC}"
 fi
-
 echo -e "| Benchmark: $status"
 echo -e "-------------------------"
