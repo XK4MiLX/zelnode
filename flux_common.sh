7504a483074406d0cb83b45fe95246aed4e65257
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-12-27 15:55:19 +0100

    fix

--- flux_common.sh
+++ flux_common.sh
@@ -2089,7 +2089,7 @@ function install_mongod() {
   fi
   #AVX with AMD64
   if [[ "$avx_check" != ""  && "$architecture" == "amd64" ]]; then
-    if [[ "$os_name" == "Ubuntu ]]; then
+    if [[ "$os_name" == "Ubuntu" ]]; then
       if [[ "$os_version" -ge "2004" ]]; then
          curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | gpg --dearmor | sudo tee /usr/share/keyrings/mongodb-archive-keyring.gpg > /dev/null 2>&1
          echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/mongodb-archive-keyring.gpg] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list > /dev/null 2>&1 
@@ -2100,7 +2100,7 @@ function install_mongod() {
          source_set=1
       fi
     fi
-    if [[ "$os_name" == "Debian ]]; then
+    if [[ "$os_name" == "Debian" ]]; then
       if [[ "$os_version" -le "9" ]]; then
          curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | gpg --dearmor | sudo tee /usr/share/keyrings/mongodb-archive-keyring.gpg > /dev/null 2>&1
          echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/mongodb-archive-keyring.gpg] https://repo.mongodb.org/apt/debian $(lsb_release -cs)/mongodb-org/6.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list > /dev/null 2>&1
