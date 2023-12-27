e06cdfbb68a239c537d5a199c5dfefdac2002b07
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-12-27 15:50:48 +0100

    fix missing then

--- flux_common.sh
+++ flux_common.sh
@@ -2076,7 +2076,7 @@ function install_mongod() {
   fi
   #ARM MongoDB 7.0
   if [[ "$architecture" == "arm64" ]]; then
-    if [[ "$os_name" == "Debian" && "$os_version" -ge "12" ]]; 
+    if [[ "$os_name" == "Debian" && "$os_version" -ge "12" ]]; then
       curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | gpg --dearmor | sudo tee /usr/share/keyrings/mongodb-archive-keyring.gpg > /dev/null 2>&1
 		  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/mongodb-archive-keyring.gpg] https://repo.mongodb.org/apt/debian $(lsb_release -cs)/mongodb-org/7.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list > /dev/null 2>&1
       source_set=1
